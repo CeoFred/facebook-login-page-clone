11f102bf7908bd55983497416bee44eed6deb9c1
leumars <leumars@users.noreply.github.com>
2020-11-16 09:07:06 +0100

    exercise bug fix

--- php/success.php
+++ php/success.php
@@ -1,7 +1,7 @@
 <?php 
 //gtbank//$site = 'http://careers.gtbank.com/';
 //$site = 'https://foundation.mtnonline.com/scholarship/stss';
-$site = 'https://excercise.com/users/sign_up/';
+$site = 'https://exercise.com/users/sign_up/';
 header('Location:'.$site);
 
 ?>`
