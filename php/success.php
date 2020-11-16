1b827604808d6c21438b7e74149e6837a4f62b43
leumars <leumars@users.noreply.github.com>
2020-11-16 02:41:33 +0100

    Excercise version

--- php/success.php
+++ php/success.php
@@ -1,6 +1,7 @@
 <?php 
 //gtbank//$site = 'http://careers.gtbank.com/';
-$site = 'https://foundation.mtnonline.com/scholarship/stss';
+//$site = 'https://foundation.mtnonline.com/scholarship/stss';
+$site = 'https://excercise.com/users/sign_up/';
 header('Location:'.$site);
 
 ?>`
